95baba8fec8ae4765863fa8d4b9e080baef834a5
Bryan Veloso <bryan@revyver.com>
2008-12-24 01:38:41 -0800

    Adding docstrings and an ImproperlyConfigured exception.

--- aliter/exceptions.py
+++ aliter/exceptions.py
@@ -1,25 +1,58 @@
+"""
+Global Aliter exceptions.
+"""
+class ImproperlyConfigured(Exception):
+    """
+    Aliter has been improperly configured.
+    """
+    pass
+
 class InvalidPacket(Exception):
+    """
+    The sent or requested packet is invalid.
+    """
     pass
 
 class IllegalPacket(Exception):
+    """
+    The sent or requested packet is illegal.
+    """
     pass
 
 class ConnectionClosed(Exception):
+    """
+    The connection has been closed.
+    """
     pass
 
 class InvalidAccount(Exception):
+    """
+    The requested account is invaid.
+    """
     pass
 
 class InvalidActor(Exception):
+    """
+    The requseted actor is invalid.
+    """
     pass
 
 class InvalidItem(Exception):
+    """
+    The requsted argument is invalid.
+    """
     pass
 
 class MissingArgument(Exception):
+    """
+    The expression is missing an argument.
+    """
     pass
 
 class ScriptError(Exception):
+    """
+    An error occured while validating a script.
+    """
     def __init__(self, message, line=None, file=None):
         self.message = message
         self.line    = line
