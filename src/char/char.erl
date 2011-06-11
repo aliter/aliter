0ba5c1990d3801c1edc988d546f71df009059c8c
Alex Suraci <i.am@toogeneric.com>
2011-06-11 13:04:16 -0400

    re-enable remote server startup now that it seems to work okay

--- src/char/char.erl
+++ src/char/char.erl
@@ -4,14 +4,42 @@
 
 -export([
     start_link/1,
+    init/1,
     install/0,
     uninstall/0,
     stop/0]).
 
 
 start_link(Config) ->
-  {_Node, Conf} = proplists:lookup(node(), Config),
-  char_srv:start_link(Conf).
+  Supervisor = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+
+  lists:foreach(
+    fun({Node, Conf}) ->
+      {host, {Host, Name}} = config:find(server.host, Conf),
+      {aliter, Aliter} = config:find(server.aliter, Conf),
+      {ok, Node} = slave:start_link(Host, Name, aliter:path(Aliter)),
+
+      supervisor:start_child(?MODULE, [Node, char_srv, start_link, [Conf]])
+    end,
+    Config
+  ),
+
+  Supervisor.
+
+
+init([]) ->
+  { ok,
+    { {simple_one_for_one, 2, 60},
+      [ { undefined,
+          {rpc, block_call, []},
+          permanent,
+          1000,
+          worker,
+          []
+        }
+      ]
+    }
+  }.
 
 
 install() -> ok.
